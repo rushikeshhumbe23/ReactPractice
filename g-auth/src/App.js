7623db620fef05565f84375677068a46ba954423
rushikeshhumbe23 <rushikeshhumbe23@gmail.com>
2023-05-20 02:07:33 +0530

    Authentication with google done

--- g-auth/src/App.js
+++ g-auth/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
-
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
 function App() {
-  return <div className="App"></div>;
+  const { user, isAuthenticated} = useAuth0();
+  console.log(user);
+  return (
+    <div className="App">
+      {isAuthenticated ? `${user.nickname}` : "Plese Login"}
+      <h1>Rushikes Humbe</h1>
+
+      <LogoutButton />
+      <LoginButton />
+    </div>
+  );
 }
 
 export default App;
--- g-auth/src/index.js
+++ g-auth/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import { Auth0Provider } from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
+  <Auth0Provider
+    domain="dev-k3i7puzmfo73kpr8.us.auth0.com"
+    clientId="afY6G1ns9S6TAgrnh7vRsOwD0MBHfuUY"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </>
+  </Auth0Provider>
 );
 
 reportWebVitals();
